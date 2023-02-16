d80cd174dabf99fae68a50a0eb67320103448ed9
MrFurphie <hfelsman@gmail.com>
2023-02-16 16:35:31 -0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,39 @@
+const verbone = ["join", "run", "laugh", "jump", "kill"];
+const adverb = ["hopelessly", "abnormally", "unethically", "viciously", "solidly"];
+const noun = ["He", "She", "They"];
+const noun2 = ["He", "She", "They"];
+
+
+function replaceNoun() {
+    let n = Math.floor(Math.random() * verbone.length);
+    let wordNoun = document.getElementById('noun');
+    document.getElementById('poutput').innerHTML = wordNoun;
+    wordNoun.innerHTML = noun[n];
+    console.log(n);
+}
+
+// function replaceNoun() {
+//     let n = Math.floor(Math.random() * verbone.length);
+//     let wordNoun = document.getElementById('noun');
+//     wordNoun.innerText = noun[n];
+// };
+// function replaceWords() {
+//     let nn = Math.floor(Math.random() * verbone.length);
+//     let wordNounTwo = document.getElementById('noun2');
+//     wordNounTwo.innerText = noun2[nn];
+// };
+// function replaceWords() {
+//     let v = Math.floor(Math.random() * verbone.length);
+//     let wordVerbOne = document.getElementById('verbone');
+//     wordVerbOne.innerText = verbone[v];
+// };
+// function replaceWords() {
+//     let av = Math.floor(Math.random() * verbone.length);
+//     let wordAdverb = document.getElementById('adverb');
+//     wordAdverb.innerText = adverb[av];
+// }
+
+// <script>
+// window.onload = function(){
+//     document.getElementById('poutput').innerHTML = wordNoun};
+//     </script>
